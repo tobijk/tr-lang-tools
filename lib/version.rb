7a4973566587353948943f2932b87b68de3477a1
Tobias Koch <tobias@tobijk.de>
2012-07-09 23:18:25 +0300

    make version a common include

---
+++ lib/version.rb
@@ -0,0 +1,9 @@
+#-*- encoding: utf-8 -*-
+#
+# Description: Viki.com Subtitle Extractor
+# Author:      Tobias Koch <tobias.koch@gmail.com>
+# License:     Public Domain
+#
+
+TRTOOLS_VERSION = "0.1.0"
+
