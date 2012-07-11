6cbba495f9019a507173a49c965b5f92fc53e531
Tobias Koch <tobias@tobijk.de>
2012-07-12 00:24:57 +0300

    add RSS reader framework

---
+++ lib/tr_rss.rb
@@ -0,0 +1,19 @@
+#-*- encoding: utf-8 -*-
+#
+# Description: RSS feed readers for sources from Turkey
+# Author:      Tobias Koch <tobias.koch@gmail.com>
+# License:     Public Domain
+#
+
+class TurkInternetRSS
+end
+
+class InternetGazeteRSS
+end
+
+class TknljRSS
+end
+
+class GazeteVatanRSS
+end
+
